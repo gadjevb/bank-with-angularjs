25f012ff3a89c66e72e1faa8bd6567c64bc37a26
Borislav Gadjev <gadjevb@gmail.com>
2017-01-06 17:34:34 +0200

    frontend: app view
    Added the basic view and layout for the app.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,8 +3,17 @@ var app = angular.module('bankApp', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'view/index_page.view.html'
+        templateUrl: 'view/login_page.view.html'
     })
+    .when('/register', {
+        templateUrl: 'view/register_page.view.html'
+     })
+    .when('/transaction', {
+        templateUrl: 'view/transaction_page.view.html'
+    })
+    .when('/account', {
+        templateUrl: 'view/account_page.view.html'
+     })
     .when('/history', {
         templateUrl: 'view/transaction_history_page.view.html'
     })
